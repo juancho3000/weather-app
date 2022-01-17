c500a5da729d6b6f5d2bc62a39cfe011bca9eaf1
juan-suarez <jsuarez0698@gmail.com>
2022-01-17 12:08:03 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import react from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="container">  
+        <div className="row">
+        <div className="col-md-3">
+                 <input type="text" className="form-control" name="city" autoComplete="off"/>
+        </div>
+        <div className="col-md-3">
+      <button className="btn-warning">Get weather for your location</button>
     </div>
+         </div>
+          </div>
+
+ 
+          
   );
 }
+
+
 
 export default App;
