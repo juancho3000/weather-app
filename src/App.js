7778ece0cd82e82743dfdaabb31920cf728da0f0
juan-suarez <jsuarez0698@gmail.com>
2022-01-18 23:43:09 -0400

    updating degrees

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import react from 'react';
+import React from 'react';
 import './App.css';
+import Weather from './app_component/weather.component';
 
-function App() {
-  return (
-        <div className="container">  
-        <div className="row">
-        <div className="col-md-3">
-                 <input type="text" className="form-control" name="city" autoComplete="off"/>
-        </div>
-        <div className="col-md-3">
-      <button className="btn-warning">Get weather for your location</button>
-    </div>
-         </div>
-          </div>
 
- 
-          
-  );
+//api call
+const API_key = "36f9a175d9c4096f8585a4df81f68181"
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      city: undefined,
+      country:undefined,
+      main:undefined,
+      celsius:undefined,
+      temp_max:undefined,
+      temp_min:undefined,
+      description:"",
+      error:false
+    };
+    this.getWeather();
+
+  }
+
+  //calculation to pass Fahrenheit to Celsius
+  calcFhrToCel(temp){
+    let cell= Math.floor(temp -32 *5 /9);
+    return cell;
+  }
+
+  getWeather= async () =>{
+    const api_call = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=caracas&appid=36f9a175d9c4096f8585a4df81f68181`
+    );
+    const response = await api_call.json();
+    console.log(response);
+
+      this.setState({
+        city:response.main.name,
+        country:response.sys.country,
+        temp:this.calcFhrToCel(response.main.temp),
+        description:response.weather.description
+      });
+  };
+
+  render(){
+    return(
+      <div className='app'>
+        <Weather
+        city={this.state.city}
+        country={this.state.country}
+        temp={this.state.temp}
+        decription={this.state.description}
+        />
+      </div>
+    );
+  }
 }
 
+/*function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-3">
+          <input type="text" className="form-control" name="city" placeholder='type city name'  autoComplete="off" />
+        </div>
+
+      
+        <div className='row'>
+        <div className="col-md-3">
+          <button className="btn-warning">Get weather for your location</button>
+        </div>
+        </div>
+      </div>
+    </div>
+
+ );
+}
+*/
 
 
 export default App;
---
+++ src/app_component/weather.component.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+const Weather = props =>{
+    return(
+
+       <div className="cards">
+       <h1>
+        {props.city}, {props.country}
+       </h1>
+       <h5 className="py-4">
+       </h5>
+       <h1 className="py-2">{props.temp}&deg;</h1>
+       
+       
+
+       <h4 className="py-3">{props.description}</h4>
+       </div>
+   
+ );
+};
+
+
+
+export default Weather;
