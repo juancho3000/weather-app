d2c243048ccfef7540d64e24f20377a962e5c181
juan-suarez <jsuarez0698@gmail.com>
2022-01-19 22:29:36 -0400

    starting with hooks

--- src/app_component/form.component.jsx
+++ src/app_component/form.component.jsx
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, {Fragment, useState} from 'react';
 
-const Form = props =>{
+const Form = () =>{
     
+const[data, setData] = useState({
+    city:''
+})
+
+const handleInputChange = (event) =>{
+    console.log('it works')
+}
 
   return (
+      <Fragment>
     <div className="container">
       <div className="row">
         <div className="col-md-3-1">
-          <input type="text" className="form-control" name="city" placeholder='type city name'  autoComplete="off" />
+          <input type="text" 
+          className="form-control" 
+          name="city" 
+          placeholder='type city name'
+          onChange={handleInputChange}  
+          autoComplete="off" />
+          
         </div>
 
       
         <div className='row'>
         <div className="col-md-3">
-           <button className="btn-warning">Get weather for your location</button>
+           <button className="btn-warning"
+            type="submit">Get weather for your location
+            </button>
         </div>
         </div>
       </div>
     </div>
-
+</Fragment>
  );
 }
 
