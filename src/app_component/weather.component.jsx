fe8ff95a3268ade4e1af403bdef33d9b333464a4
juan-suarez <jsuarez0698@gmail.com>
2022-01-21 10:53:13 -0400

    fixing styles of cards

--- src/app_component/weather.component.jsx
+++ src/app_component/weather.component.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 const Weather = props =>{
     return(
        <div className="cards">
-       <h1>{props.city}, {props.country}</h1>
-       
+       <h1>{props.city} {props.country}</h1>
+
        {props.temp ?   (<h1 className="py-2">{props.temp}&deg;</h1>):null
        }
     
